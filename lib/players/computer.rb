fa66b94f6d97f47f99e64a52efa2abaa74239e96
Kevin W <kjoewill@gmail.com>
2018-09-07 04:03:23 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,13 +1,10 @@
 module Players
   class Computer < Player
     
-    
-    WIN_COMBOS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
-  
-    
     def move(board)
       brain = ComputerBrain.new
       brain.move(board)
     end
   end
+  
 end
